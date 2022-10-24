02b3d0635189b46da0d65a253f2b6554bfd0f4da
Andrey Yurjev <yurjevad@gmail.com>
2022-10-24 14:37:46 +0400

    Add project files.

---
+++ tests/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace tests
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var list = new Node(new byte[] { 1 }
+                    , new Node(new byte[] { 2 }
+                        , new Node(new byte[] { 3 }
+                            , new Node(new byte[] { 4 }
+                                , new Node(new byte[] { 5 }, null))))
+                );
+            Print(list);
+            Reverse(ref list);
+            Print(list);
+        }
+
+        public static void Print(Node? current)
+        {
+            while (null != current)
+            {
+                var data = current.Data ?? new byte[0];
+                Console.Write($"[{string.Join(',', data)}]");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public static void Reverse(ref Node? head)
+        {
+            if (null != head)
+            {
+                Node? previous = null;
+                Node? current = head;
+                Node? next = null;
+
+                while (current.Next != null)
+                {
+                    next = current.Next;
+                    current.Next = previous;
+                    previous = current;
+                    current = next;
+                }
+                current.Next = previous;
+                head = current;
+            }
+        }
+
+        public class Node
+        {
+            public byte[]? Data { get; set; }
+            public Node? Next { get; set; }
+            public Node(byte[]? data, Node? next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+    }
+}
